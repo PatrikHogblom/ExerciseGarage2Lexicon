c6357944abeec63e8f8d249e0ae7a30117a90ce9
FelixEdenborgh <Felix@edenborgh.se>
2024-04-17 11:26:20 +0200

    First Model Vehicle

---
+++ ExerciseGarage2Lexicon/Models/ParkedVehicle.cs
@@ -0,0 +1,15 @@
+﻿namespace ExerciseGarage2Lexicon.Models
+{
+    public class ParkedVehicle
+    {
+        public int Id { get; set; }
+        public string VehicleType { get; set; }
+        public string RegistrationNumber { get; set; } // måste vara olika "ingen får vara samma som den andra"
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int NumberOfWheels { get; set; } // får inte vara negativt nummer
+
+        public DateTime ArrivalTime { get; set; } // ska inte gå att editera eller manipulera
+    }
+}
