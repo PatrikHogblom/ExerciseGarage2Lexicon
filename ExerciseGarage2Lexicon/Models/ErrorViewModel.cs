2d37fb704c1ee110a22675c0dbc8603929309534
PatrikHogblom <patrik.hogblom@live.se>
2024-04-17 10:48:24 +0200

    Add project files.

---
+++ ExerciseGarage2Lexicon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExerciseGarage2Lexicon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
