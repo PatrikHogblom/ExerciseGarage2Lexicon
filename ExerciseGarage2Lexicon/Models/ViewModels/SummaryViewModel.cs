4410cb559ebf121d8e203881a9058fa87f58b2fb
Tina Bruce <tina@fishface.se>
2024-04-22 11:37:38 +0200

    Change time parked to show days, hours and minutes

--- ExerciseGarage2Lexicon/Models/ViewModels/SummaryViewModel.cs
+++ ExerciseGarage2Lexicon/Models/ViewModels/SummaryViewModel.cs
@@ -13,7 +13,9 @@ namespace ExerciseGarage2Lexicon.Models.ViewModels
         [Display(Name = "Arrival Time")]
         public DateTime ArrivalTime { get; set; }
 
-        [Display(Name = "Minutes Parked")]
-        public double TotalParkingTime { get; set; } 
+        [Display(Name = "Time Parked")]
+        public TimeSpan TotalParkingTime { get; set; }
+
+        public string DisplayTime => $"{TotalParkingTime.Days} days, {TotalParkingTime.Hours} h {TotalParkingTime.Minutes} min";
     }
 }
